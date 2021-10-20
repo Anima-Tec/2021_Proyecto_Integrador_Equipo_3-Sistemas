65fa002e884fe42d7d04c00892a4f3b06aea59a2
Facundo Correa <facorrea179@gmail.com>
2021-10-20 09:28:07 -0300

    control center improoves

--- control_center.sh
+++ control_center.sh
@@ -1,21 +1,29 @@
 #!/bin/sh
 
-echo "1)Administrar usuarios    2)Comprobar recursos del sistema"
-echo "3)Administrar backups     4)Solucionar error SQLSTATE 2002"
-read -p "Seleccione una opcion" option
+echo "1)Manage users    2)Check system resources"
+echo "3)Manage backups  4)Fix SQLSTATE 2002 (until system reboot)"
+echo "5)Fix SQLSTATE 2002 (permanent)"
+read -p "Select an option: " option
 case $option in
 1)
 ./users_crud.sh
 ;;
 2)
-glances --disable-sensors --hide-kernel-threads --disable-network --disable-hddtemp --disable-process --disable-log --disable-docker
+glances  --disable-sensors --hide-kernel-threads --disable-network --disable-hddtemp --disable-process --disable-log --disable-docker --disable-raid --disable-mem --disable-swap --tree --fs-free-space --disable-diskio
 ;;
 3)
-read -p "Ingrese el directorio o archivo a respaldar (ruta absoluta): " inputBackupFile
+read -p "Insert folder or file to backup (absolute path): " inputBackupFile
 ./backups.sh $inputBackupFile
 ;;
 4)
 ./permissionsSQLSTATE2002.sh
+systemctl enable SQLSTATE2002
+systemctl start SQLSTATE2002
+;;
+5)
+echo "Trying to install daemon..."
+cp SQLSTATE2002.service /etc/systemd/system/SQLSTATE2002.service
+echo "Task finished."
 ;;
 *)
 ./control_center.sh
